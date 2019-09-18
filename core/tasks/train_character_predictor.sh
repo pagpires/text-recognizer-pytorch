b2f0d551e395bc96930ec3fee9225269a584bc91
pagpires <hongxinyupires@gmail.com>
2019-09-17 23:52:06 -0400

    ADD LeNet

--- core/tasks/train_character_predictor.sh
+++ core/tasks/train_character_predictor.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python training/run_experiment.py --save '{"dataset": "EmnistDataset", "model": "CharacterModel", "network": "mlp", "train_args": {"batch_size": 1024}}'
+python training/run_experiment.py --save '{"dataset": "EmnistDataset", "model": "CharacterModel", "network": "lenet", "train_args": {"batch_size": 1024}}'
--- core/text_recognizer/networks/lenet.py
+++ core/text_recognizer/networks/lenet.py
@@ -1,19 +1,40 @@
 """LeNet network."""
 from typing import Tuple
-
 from torch import nn
-
-# import tensorflow as tf
-# from tensorflow.keras.layers import Conv2D, Dense, Dropout, Flatten, Lambda, MaxPooling2D
-# from tensorflow.keras.models import Sequential, Model
-
 
 def lenet(input_shape: Tuple[int, ...], output_shape: Tuple[int, ...]) -> nn.Module:
     """Return LeNet Keras model."""
     num_classes = output_shape[0]
+    input_h = input_shape[0]
 
-    # Your code below (Lab 2)
+    class LeNet(nn.Module):
+        def __init__(self):
+            super(LeNet, self).__init__()
+            layers = []
+            layers.append(nn.Conv2d(1, 32, 3))
+            layers.append(nn.ReLU())
+            layers.append(nn.Conv2d(32, 64, 3))
+            layers.append(nn.ReLU())
+            layers.append(nn.MaxPool2d(2))
+            layers.append(nn.Dropout2d(0.2))
+            self.conv_layer = nn.Sequential(*layers)
+            layers = []
+            self.output_h = (input_h-4)//2
+            layers.append(nn.Linear(64*self.output_h*self.output_h, 128))
+            layers.append(nn.ReLU())
+            layers.append(nn.Dropout(0.2))
+            layers.append(nn.Linear(128, num_classes))
+            layers.append(nn.Softmax(dim=1))
+            self.mlp_layer = nn.Sequential(*layers)
+        
+        def forward(self, x):
+            if len(x.shape) < 4:
+                x = x.unsqueeze(dim=1)
+            x = self.conv_layer(x)
+            x = x.view(-1, 64 * self.output_h * self.output_h)
+            x = self.mlp_layer(x)
+            return x
 
-    # Your code above (Lab 2)
+    model = LeNet()
 
     return model
