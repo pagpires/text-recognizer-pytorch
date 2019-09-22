42272085e5f9e7d3dd8a609e5e057af06e5225dc
pagpires <hongxinyupires@gmail.com>
2019-09-22 18:59:43 -0400

    MOD prepare_experiments

--- core/tasks/prepare_sample_experiments.sh
+++ core/tasks/prepare_sample_experiments.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-# NOTE this is to run (not create) multiple experiment specified from sample.json 
+
+# NOTE this is to create multiple experiment specified from sample.json and output to stdio
+# Thus we can output it to a file or pipe it to a executor
 python training/prepare_experiments.py training/experiments/sample.json
