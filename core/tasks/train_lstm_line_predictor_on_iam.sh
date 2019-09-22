3317a7622bffb6741c828252891a098269b0bf23
pagpires <hongxinyupires@gmail.com>
2019-09-22 19:16:49 -0400

    ADD lab5 v0 template

---
+++ core/tasks/train_lstm_line_predictor_on_iam.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python training/run_experiment.py --save '{"dataset": "IamLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc"}'
--- core/text_recognizer/tests/test_line_predictor.py
+++ core/text_recognizer/tests/test_line_predictor.py
@@ -8,6 +8,7 @@ import numpy as np
 
 import text_recognizer.util as util
 from text_recognizer.line_predictor import LinePredictor
+from text_recognizer.datasets import IamLinesDataset
 
 SUPPORT_DIRNAME = Path(__file__).parents[0].resolve() / 'support'
 
@@ -40,3 +41,16 @@ class TestEmnistLinePredictorVariableImageWidth(unittest.TestCase):
             print(f'Pred: "{pred}" | Confidence: {conf} | True: {true} | Edit distance: {edit_distance}')
             self.assertLess(edit_distance, 0.2)
 
+class TestIamLinePredictor(unittest.TestCase):
+    def test_filename(self):  # pylint: disable=R0201
+        predictor = LinePredictor(IamLinesDataset)
+
+        for filename in (SUPPORT_DIRNAME / 'iam_lines').glob('*.png'):
+            pred, conf = predictor.predict(str(filename))
+            true = filename.stem
+            if pred:
+                edit_distance = editdistance.eval(pred, true) / len(pred)
+            else:
+                edit_distance = 0
+            print(f'Pred: "{pred}" | Confidence: {conf} | True: {true} | Edit distance: {edit_distance}')
+            # self.assertLess(edit_distance, 0.2)
