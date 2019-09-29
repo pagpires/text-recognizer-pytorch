fa0f872a5db0ddd66e99f3b7d27578adb1843669
pagpires <hongxinyupires@gmail.com>
2019-09-29 08:41:27 -0400

    ADD lab7 starter

--- core/text_recognizer/datasets/__init__.py
+++ core/text_recognizer/datasets/__init__.py
@@ -3,4 +3,5 @@ from .emnist_lines_dataset import EmnistLinesDataset
 from .iam_lines_dataset import IamLinesDataset
 from .iam_dataset import IamDataset
 from .iam_paragraphs_dataset import IamParagraphsDataset
+from .fsdl_handwriting_dataset import FsdlHandwritingDataset
 
